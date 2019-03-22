e3b60454a4aa8fcfcf6dcdf52b957ec7ca6f33bf
RafaelBloise <rafael_bloise@hotmail.com>
2019-03-22 00:09:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aulaframelayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.aulaframelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login (View view){
+        FrameLayout fl = findViewById(R.id.fl2);
+        fl.setVisibility(View.VISIBLE);
+    }
+
+}
